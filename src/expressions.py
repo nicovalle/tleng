7fd5f58cc072e2994b2da805f66c7d2383310aaf
Nicolás Vallejo <nico@MacBook-Pro-de-Nicolas.local>
2015-11-25 20:28:07 -0300

    toqueteo de codigo. sigo sin arreglar lo del divide

--- src/expressions.py
+++ src/expressions.py
@@ -61,7 +61,7 @@ class Divide(object):
 		self.right.y = self.y
 		self.left.operate()
 		self.right.operate()
-		moveY(self.right, self.right.height)
+		moveY(self.right, self.right.height / 2)
 		self.width = max(self.left.width, self.right.width)
 		self.height = self.left.height + self.right.height #+ 0.28 * self.scale
 		
